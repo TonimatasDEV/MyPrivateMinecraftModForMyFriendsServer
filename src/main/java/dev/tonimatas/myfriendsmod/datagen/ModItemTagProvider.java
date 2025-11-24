e793a5c06f344185149f69da5d07f175de8af27a
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-11-24 20:00:17 +0100

    fix: Make items enchantable

---
+++ src/main/java/dev/tonimatas/myfriendsmod/datagen/ModItemTagProvider.java
@@ -0,0 +1,31 @@
+package dev.tonimatas.myfriendsmod.datagen;
+
+import dev.tonimatas.myfriendsmod.MyFriendsMod;
+import dev.tonimatas.myfriendsmod.register.ModItems;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends ItemTagsProvider {
+    public ModItemTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider,
+                              CompletableFuture<TagLookup<Block>> blockTags, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, blockTags, MyFriendsMod.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.@NotNull Provider provider) {
+        tag(ItemTags.SWORDS)
+                .add(ModItems.BLOOD_SWORD.get())
+                .add(ModItems.JADE_SWORD.get());
+
+        tag(ItemTags.AXES)
+                .add(ModItems.VOID_AXE.get());
+    }
+}
